a0ccb019500ed13311626e984ed177f54ae7c082
CeVaRu99 <juliocvr14@gmail.com>
2021-11-17 12:22:58 -0700

    Updated

--- BloqueanteNodeJS/Bloqueante.js
+++ BloqueanteNodeJS/Bloqueante.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-console.log('\nAbriendo Archivo ...')
+console.log('\nAbriendo Archivo...')
 
 var content = fs.readFile('archivo.txt','utf8', function(error,content){
 console.log(content)
